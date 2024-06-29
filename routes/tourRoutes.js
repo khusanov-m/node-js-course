499e93575669f872e8c300cd58707874aa9d7d29
khusanov-m <khusanov.it.tech@gmail.com>
2024-06-30 02:16:27 +0500

    feat: add error handlers, global, server, router, lib

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.use(authController.protect);
 
 router
   .route('/top-5-cheap')
@@ -19,6 +22,8 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
-
+  .delete(
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour,
+  );
 module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,22 @@ mongoose.connect(DB).then(() => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXECPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
